df9344694ae66358f38f864ca71a3a9c5c811435
Sambandh47 <90717010+Sambandh47@users.noreply.github.com>
2021-09-16 16:57:36 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,89 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  for i in arr do
+    sum = sum + i
+  end
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length() == 0
+    return 0
+  elsif arr.length() == 1
+    return arr[0]
+  else
+    max_array = arr.max(2){ |a, b| a<=>b}
+    return sum(max_array)
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length == 0 or arr.length() == 1
+    return false
+  end
+  (0..arr.length-1).any? do |i|
+    arr[i+1..arr.length].any? { |x| x + arr[i] == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  a1 = [65,69,73,79,85,97,101,105,111,117]
+  first_letter_of_string = s[0]
+  ascii_value = first_letter_of_string.ord()
+  if ((ascii_value >= 65 and ascii_value<= 90) or (ascii_value>=97 and ascii_value<=122))
+    if a1.include?(ascii_value)
+      return false
+    else
+      return true
+    end
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  if s.count('10') != s.size
+    return false
+  end
+  if s[s.size - 1]=='0' and s[s.size - 2]=='0'
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    @isbn = isbn
+    @price = price
+    if isbn.length==0 or price<=0
+      raise ArgumentError
+    end
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    return "$%.2f" % price
+  end
 end
